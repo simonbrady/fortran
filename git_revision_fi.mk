2234b0ea05cb2094c09c2c0c0867f4fd695aa87c
Simon Brady <simon@hikari.org.nz>
2015-06-10 22:56:15 +1200

    git_revision_fi.mk: do something sensible when building outside a git repository

--- git_revision_fi.mk
+++ git_revision_fi.mk
@@ -3,11 +3,8 @@ git_revision.fi: git_revision
 	
 # Prevent unnecessary recompilation by only modifying git_revision.fi when the revision changes 
 git_revision:
-	git rev-parse --short HEAD | xargs printf "character(len=*), parameter :: git_revision = '%s" > $@.tmp
-ifeq ($(DEBUG), 1)
-	echo -n "-debug" >> $@.tmp
-endif
-	echo "'" >> $@.tmp
+	rev=$$(git rev-parse --short HEAD 2>/dev/null || echo "unknown"); \
+	printf "character(len=*), parameter :: git_revision = '%s'\n" $${rev} > $@.tmp; \
 	if cmp -s $@.tmp $@.fi; then \
 		rm $@.tmp; \
 	else \
